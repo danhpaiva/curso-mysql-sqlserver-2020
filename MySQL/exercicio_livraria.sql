c2253addde6e06052ceb68ac8fb17275af902c96
Daniel Paiva <danpaiva@live.com>
2020-07-03 17:36:49 -0300

    Add comentários nas query's

--- MySQL/exercicio_livraria.sql
+++ MySQL/exercicio_livraria.sql
@@ -24,28 +24,33 @@ INSERT INTO LIVROS VALUES('Cavaleiro Real','Ana Claudia','F',465,'Atlas',49.9,'R
 						,('Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',130,'RS',2018)
 						,('O poder da mente','Clara Mafra','F',120,'Continental',56,'SP',2017);
 						
+/*Trazer todos os dados.*/					
 SELECT * 
 FROM LIVROS;
 
+/*Trazer o nome do livro e o nome da editora*/
 SELECT NOME_LIVRO AS 'NOME DO LIVRO', NOME_EDITORA AS 'NOME DA EDITORA'
 FROM LIVROS;
 
+/*Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino.*/
 SELECT NOME_LIVRO AS 'NOME DO LIVRO', UF_EDITORA AS 'ESTADO'
 FROM LIVROS
 WHERE SEXO = 'M';
 
+/*Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino.*/
 SELECT NOME_LIVRO AS 'NOME DO LIVRO', NUMERO_PAGINAS AS 'PAGINAS'
 FROM LIVROS
 WHERE SEXO = 'F';
 
-SELECT VALOR_LIVRO AS 'VALOR DO LIVRO'
+/*Trazer os valores dos livros das editoras de São Paulo.*/
+SELECT NOME_LIVRO AS 'NOME DO LIVRO', VALOR_LIVRO AS 'VALOR DO LIVRO'
 FROM LIVROS
 WHERE UF_EDITORA = 'SP';
 
+/*Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro (Questão Desafio).*/
 SELECT NOME_AUTOR AS 'NOME DO AUTOR', SEXO
 FROM LIVROS
 WHERE SEXO = 'M' AND (UF_EDITORA = 'RJ' OR UF_EDITORA = 'SP');
 
 /* BÔNUS: COMANDO PARA SABER A DATABASE CORRENTE*/
-
 STATUS;
