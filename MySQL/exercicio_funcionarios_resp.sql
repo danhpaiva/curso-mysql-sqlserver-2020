0655de25a689cf572a8dbc12657578298f030a24
Daniel Paiva <danpaiva@live.com>
2020-07-08 11:52:22 -0300

    RESPOSTAS

--- MySQL/exercicio_funcionarios_resp.sql
+++ MySQL/exercicio_funcionarios_resp.sql
@@ -12,6 +12,16 @@ OR DEPARTAMENTO = 'ROUPAS';
 --Traga os funcionarios do sexo masculino
 --ou os funcionarios que trabalhem no setor Jardim
 
+SELECT NOME AS 'NOME DO FUNCIONARIO', SEXO, DEPARTAMENTO
+FROM FUNCIONARIOS
+WHERE SEXO = 'MASCULINO'
+OR DEPARTAMENTO = 'JARDIM';
+
 --O gestor de marketing pediu a lista das funcionarias (AS) = FEMININO que trabalhem no departamento 
 --de filmes ou no departamento lar. Ele necessita enviar um email para as colaboradoras
 --desses dois setores. OR +  AND
+
+SELECT NOME AS 'NOME DAS FUNCIONARIAS', SEXO, DEPARTAMENTO
+FROM FUNCIONARIOS
+WHERE SEXO = 'FEMININO'
+AND (DEPARTAMENTO = 'JARDIM' OR DEPARTAMENTO = 'LAR');
